1cc8e9a7fc36033aaf0bf1c2fc7661716f2a452b
mishkamn <bayartsem@gmail.com>
2020-02-20 18:53:31 -0500

    first push

---
+++ src/lightbar_driver_application.cpp
@@ -0,0 +1,139 @@
+/*
+ * Copyright (C) 2019-2020 LEIDOS.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+#include "lightbar_driver/lightbar_driver_application.h"
+#include <ros/ros.h>
+
+namespace lightbar_driver
+{
+
+LightBarApplication::LightBarApplication(int argc, char **argv) : cav::DriverApplication(argc, argv, "lightbar")
+{
+    cav_msgs::DriverStatus status;
+    status.status = cav_msgs::DriverStatus::OFF;
+    status.lightbar = true;
+    setStatus(status);
+    lightbar_ctrl_.turnOffAll();
+    ROS_INFO_STREAM("LightBar Driver Started!");
+}
+
+void LightBarApplication::initialize() 
+{
+    lightbar_api_nh_.reset(new ros::NodeHandle("lightbar"));
+
+    // Setup the ROS API
+    std::string node_name = ros::this_node::getName();
+    api_.clear();
+    
+    // Topics 
+    lightbar_pub_ = lightbar_api_nh_->advertise<cav_msgs::LightBarStatus>("light_bar_status", 10);
+    api_.push_back(lightbar_pub_.getTopic());
+
+    // Services
+    get_lights_srv_ = lightbar_api_nh_->advertiseService("get_lights", &LightBarApplication::get_lights_cb, this);
+    api_.push_back(get_lights_srv_.getService());
+
+    set_lights_srv_ = lightbar_api_nh_->advertiseService("set_lights", &LightBarApplication::set_lights_cb, this);
+    api_.push_back(set_lights_srv_.getService());
+
+    // Timer loop
+    status_publisher_timer_ = nh_->createWallTimer(ros::WallDuration(ros::Rate(2)),&LightBarApplication::statusUpdateTimerCB, this);
+    status_publisher_timer_.start();
+
+    // Driver Status set to Operational
+    cav_msgs::DriverStatus status = getStatus();
+    status.status = cav_msgs::DriverStatus::OPERATIONAL;
+    setStatus(status);
+
+    spin_rate = 50;
+}
+
+bool LightBarApplication::get_lights_cb(cav_srvs::GetLightsRequest&, cav_srvs::GetLightsResponse &resp) {
+    static auto ON = static_cast<cav_msgs::LightBarStatus::_flash_type>(cav_msgs::LightBarStatus::ON);
+    static auto OFF = static_cast<cav_msgs::LightBarStatus::_flash_type>(cav_msgs::LightBarStatus::OFF);
+    
+    // TODO (Optional) Make LightBarStatus service to accept LightBarID (front or back)
+    // to give back status. With current design, we keep both lightbars with same state.
+    LightBar curr_front = lightbar_ctrl_.getState(frontID);
+    //LightBar curr_back = lightbar_ctrl_.getState(backID);
+
+    resp.status.yellow_solid    = curr_front.light_by_id[YellowDimOn]   ? ON : OFF;
+    resp.status.left_arrow  = curr_front.light_by_id[LeftArrowOn]       ? ON : OFF;
+    resp.status.right_arrow = curr_front.light_by_id[RightArrowOn]      ? ON : OFF;
+    resp.status.green_solid = curr_front.light_by_id[GreenSolidOn]      ? ON : OFF;
+    resp.status.flash = curr_front.light_by_id[YellowFlashOn]           ? ON : OFF;
+    resp.status.sides_solid = curr_front.light_by_id[YellowSidesOn]     ? ON : OFF;
+
+    return true;
+}
+
+bool LightBarApplication::set_lights_cb(cav_srvs::SetLightsRequest &req, cav_srvs::SetLightsResponse&) 
+{
+    using cav_msgs::LightBarStatus;
+
+    // TODO (Optional) Make LightBarStatus service to accept LightBarID (front or back)
+    // to give back status. With current design, we keep both lightbars with same state.
+
+    LightBar curr_front = lightbar_ctrl_.getState(frontID);
+    //LightBar curr_back = lightbar_ctrl_.getState(backID);
+    
+    curr_front.light_by_id[YellowDimOn]  = req.set_state.yellow_solid   == LightBarStatus::ON;
+    curr_front.light_by_id[LeftArrowOn]  = req.set_state.left_arrow     == LightBarStatus::ON;
+    curr_front.light_by_id[RightArrowOn] = req.set_state.right_arrow    == LightBarStatus::ON;
+    curr_front.light_by_id[GreenSolidOn] = req.set_state.green_solid    == LightBarStatus::ON;
+    curr_front.light_by_id[YellowFlashOn]= req.set_state.flash          == LightBarStatus::ON;
+    curr_front.light_by_id[YellowSidesOn]= req.set_state.sides_solid    == LightBarStatus::ON;
+
+    lightbar_ctrl_.setState(frontID, curr_front);
+    lightbar_ctrl_.setState(backID, curr_front);
+    //lightbar_ctrl_.setState(backID, curr_back);
+
+    return true;
+}
+void LightBarApplication::statusUpdateTimerCB(const ros::WallTimerEvent &) 
+{
+    cav_msgs::LightBarStatus light_bar_msg;
+    // With current design, basing the lightbar status only off of front lightbar.
+    LightBar curr_front = lightbar_ctrl_.getState(frontID);
+    //LightBar curr_back = lightbar_ctrl_.getState(backID);
+    
+    light_bar_msg.yellow_solid  = curr_front.light_by_id[YellowDimOn];         
+    light_bar_msg.left_arrow    = curr_front.light_by_id[LeftArrowOn];         
+    light_bar_msg.right_arrow   = curr_front.light_by_id[RightArrowOn];        
+    light_bar_msg.green_solid   = curr_front.light_by_id[GreenSolidOn];        
+    light_bar_msg.flash         = curr_front.light_by_id[YellowFlashOn];         
+    light_bar_msg.sides_solid   = curr_front.light_by_id[YellowSidesOn];   
+
+    lightbar_pub_.publish(light_bar_msg); 
+}
+
+void LightBarApplication::post_spin() 
+{
+    // bypassing any diagnostic and error handling stuff
+    auto status = getStatus();
+    status.status = cav_msgs::DriverStatus::OPERATIONAL;
+    setStatus(status);
+}
+
+void LightBarApplication::shutdown()
+{
+    cav_msgs::DriverStatus status = getStatus();
+    status.status = cav_msgs::DriverStatus::OFF;
+    setStatus(status);
+    ROS_WARN_STREAM("LightBar Driver Shut Down");
+}
+
+} // namespace lightbar_driver
---
+++ test/test_lightbar_driver_controller.cpp
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) 2019-2020 LEIDOS.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+
+#include <gtest/gtest.h>
+#include <lightbar_driver/lightbar_driver_controller.h>
+
+namespace lightbar_driver
+{
+
+TEST(LightParamTest, test)
+{
+    // Initialize
+    LightBar on_bar(ON,ON,ON,ON,ON,ON);
+    LightBar off_bar;
+    // testing ==
+    ASSERT_FALSE(on_bar == off_bar);
+    // testing !=
+    ASSERT_TRUE(on_bar != off_bar);
+    on_bar.light_by_id[GreenSolidOn] = OFF;
+    ASSERT_FALSE(on_bar == off_bar);
+    on_bar.light_by_id[GreenSolidOn] = ON;
+    // off_bar is all on now
+    // on_bar is all on now as well
+    off_bar = on_bar;
+    // testing assignment= operator
+    ASSERT_TRUE(off_bar == on_bar);
+    // testing operator()
+    on_bar.light_by_id[GreenSolidOn] = OFF;
+    ASSERT_FALSE(off_bar == on_bar);
+    off_bar(OFF,ON,ON,ON,ON,ON);
+    ASSERT_TRUE(off_bar == on_bar);
+}
+
+TEST (GetStateTest, testException)
+{
+    LightBarController lbc;
+    LightBar off_state;
+    // Without hardware, should not be able to connect
+    ASSERT_THROW(lbc.getState(frontID), CURL_EASY_PERFORM_ERROR);
+    // There should not be any change in the local copy
+    // lbc is created with both lights off by default
+    ASSERT_TRUE(off_state == lbc.getStateLocal(frontID));
+    ASSERT_TRUE(off_state == lbc.getStateLocal(backID));
+}
+
+TEST (SetStateTest, testException)
+{
+    LightBarController lbc;
+    LightBar off_state, on_state(ON,ON,ON,ON,ON,ON);
+    // Without hardware, should not be able to connect
+    ASSERT_THROW(lbc.setState(frontID, on_state), CURL_EASY_PERFORM_ERROR);
+    // There should not be any change in the local copy
+    // lbc is created with both lights off by default
+    ASSERT_TRUE(off_state == lbc.getStateLocal(frontID));
+    ASSERT_TRUE(off_state == lbc.getStateLocal(backID));
+}
+/*
+TODO Need mock localhost listener to implement this.
+TEST (SendRequestTest, test1)
+{
+    LightBarController lbc;
+    LightBar off_state, on_state(ON,ON,ON,ON,ON,ON);
+    std::string post_data = "", http_request = "api/v1/employees";
+    // Without hardware, should not be able to connect
+    ASSERT_THROW(lbc.sendRequest(frontID, on_state), CURL_EASY_PERFORM_ERROR);
+    // There should not be any change in the local copy
+    // lbc is created with both lights off by default
+    ASSERT_TRUE(off_state == lbc.getStateLocal(frontID));
+    ASSERT_TRUE(off_state == lbc.getStateLocal(backID));
+}
+*/
+TEST (UpdateStatusTest, test1)
+{
+    LightBarController lbc;
+    // Sample xml response from GET request
+
+    // test if updateStatus can update using XML string, and
+    // test if it updates light IDs only are in the string
+	std::string response_str = "<ADAM-6256 status=\"OK\"><DO><ID>0</ID><VALUE>1</VALUE></DO><DO><ID>2</ID><VALUE>0</VALUE></DO><DO><ID>3</ID><VALUE>0</VALUE></DO></ADAM-6256> ";
+    LightBar response_front(OFF, ON, ON, OFF, OFF, OFF);
+    LightBar response_back;
+	// Update according to string XML response
+	lbc.updateStatus(response_str);
+    LightBar front = lbc.getStateLocal(frontID);
+    LightBar back = lbc.getStateLocal(backID);
+    ASSERT_TRUE (front == response_front);
+    ASSERT_TRUE (back == response_back);
+
+    // test if updateStatus can update two lightbars at the same time
+    response_str = "<ADAM-6256 status=\"OK\"><DO><ID>0</ID><VALUE>1</VALUE></DO><DO><ID>1</ID><VALUE>0</VALUE></DO><DO><ID>9</ID><VALUE>0</VALUE></DO><DO><ID>8</ID><VALUE>0</VALUE></DO></ADAM-6256> ";
+    response_front(OFF, ON, ON, OFF, OFF, OFF);
+    response_back(ON,OFF,OFF,OFF,OFF,OFF);
+    lbc.updateStatus(response_str);
+    front = lbc.getStateLocal(frontID);
+    back = lbc.getStateLocal(backID);
+    ASSERT_TRUE (front == response_front);
+    ASSERT_TRUE (back == response_back);
+}
+
+} //namespace lightbar_driver
+
+
+
+
+
